59025fb1557b9614c97ec02d95d2c914ed11d79f
Aleks-Kas <spawn_kas@mail.ru>
2023-06-07 14:10:16 +0300

    v_0.052 Сделано - Debug Tool. Нужно создать модель Автор и связать с объявой и ответами

--- messageboard/board/urls.py
+++ messageboard/board/urls.py
@@ -3,7 +3,6 @@ from django.urls import path, re_path
 from .views import *
 
 urlpatterns = [
-    # path('', index, name='home'),
     path('', BoardHome.as_view(), name='home'),
     path('about/', about, name='about'),
     path('add_announce/', AddAnnounce.as_view(), name='add_announce'),
@@ -12,6 +11,5 @@ urlpatterns = [
     path('logout/', logout_user, name='logout'),
     path('register/', RegisterUser.as_view(), name='register'),
     path('post/<slug:post_slug>/', ShowPost.as_view(), name='post'),
-    # path('category/<int:cat_id>/', show_category, name='category'),
     path('category/<slug:cat_slug>/', AnnounceCategory.as_view(), name='category'),
 ]
--- messageboard/board/views.py
+++ messageboard/board/views.py
@@ -21,6 +21,9 @@ class BoardHome(DataMixin, ListView):
         c_def = self.get_user_context(title='Главная страница')
         return dict(list(context.items()) + list(c_def.items()))
 
+    def get_queryset(self):
+        return Announce.objects.all().select_related('cat')
+
 
 def about(request):
     return render(request, 'board/about.html', {'menu': menu, 'title': 'О сайте'})
@@ -70,13 +73,14 @@ class AnnounceCategory(DataMixin, ListView):
     allow_empty = False
 
     def get_queryset(self):
-        return Announce.objects.filter(cat__slug=self.kwargs['cat_slug'])
+        return Announce.objects.filter(cat__slug=self.kwargs['cat_slug']).select_related('cat')
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data(**kwargs)  # получаем контекст уже существующий
+        c = Category.objects.get(slug=self.kwargs['cat_slug'])
         c_def = self.get_user_context(
-            title='Категория - ' + str(context['posts'][0].cat),
-            cat_selected=context['posts'][0].cat_id
+            title='Категория - ' + str(c.name),
+            cat_selected=c.pk
         )
         return dict(list(context.items()) + list(c_def.items()))
 
