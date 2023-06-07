b6a5042a40c24085f30776472c3b730bd15c0ddb
Aleks-Kas <spawn_kas@mail.ru>
2023-06-07 15:07:10 +0300

    v_0.061 Сделано - кэш. Нужно создать модель Автор и связать с объявой и ответами

--- messageboard/board/urls.py
+++ messageboard/board/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path, re_path
+from django.views.decorators.cache import cache_page
 
 from .views import *
 
 urlpatterns = [
-    path('', BoardHome.as_view(), name='home'),
+    path('', cache_page(60 * 10)(BoardHome.as_view()), name='home'),
     path('about/', about, name='about'),
     path('add_announce/', AddAnnounce.as_view(), name='add_announce'),
     path('find_announce/', find_announce, name='find_announce'),
@@ -11,5 +12,5 @@ urlpatterns = [
     path('logout/', logout_user, name='logout'),
     path('register/', RegisterUser.as_view(), name='register'),
     path('post/<slug:post_slug>/', ShowPost.as_view(), name='post'),
-    path('category/<slug:cat_slug>/', AnnounceCategory.as_view(), name='category'),
+    path('category/<slug:cat_slug>/', cache_page(60 * 10)(AnnounceCategory.as_view()), name='category'),
 ]
