313eeb8a997a75c0f453577b2fc5b20cfd52e8a7
Aleks-Kas <spawn_kas@mail.ru>
2023-06-30 08:48:32 +0300

    v_0.082 Сделан фильтр своих сообщений Возможно - нужно создать модель Автор и связать с объявой и ответами

--- messageboard/board/urls.py
+++ messageboard/board/urls.py
@@ -4,7 +4,7 @@ from django.views.decorators.cache import cache_page
 from .views import *
 
 urlpatterns = [
-    path('', cache_page(60 * 10)(BoardHome.as_view()), name='home'),
+    path('', cache_page(60 * 1)(BoardHome.as_view()), name='home'),
     path('about/', about, name='about'),
     path('add_announce/', AddAnnounce.as_view(), name='add_announce'),
     # path('find_announce/', find_announce, name='find_announce'),
@@ -13,5 +13,5 @@ urlpatterns = [
     path('logout/', logout_user, name='logout'),
     path('register/', RegisterUser.as_view(), name='register'),
     path('post/<slug:post_slug>/', ShowPost.as_view(), name='post'),
-    path('category/<slug:cat_slug>/', cache_page(60 * 10)(AnnounceCategory.as_view()), name='category'),
+    path('category/<slug:cat_slug>/', cache_page(60 * 1)(AnnounceCategory.as_view()), name='category'),
 ]
--- messageboard/board/views.py
+++ messageboard/board/views.py
@@ -130,18 +130,17 @@ class AnnounceAuthor(DataMixin, ListView):
     def get_queryset(self):
         print('get_queryset')
         print(self)
-        # return Announce.objects.filter(cat__slug=self.kwargs['cat_slug']).select_related('cat')
-        # return Announce.objects.filter(cat__slug=self.kwargs['cat_slug']).select_related('cat')
-        # return Announce.objects.all()
+        queryset = Announce.objects.filter(author=self.request.user)
+        return queryset
 
-    def get_context_data(self, *, object_list=None, **kwargs):
-        context = super().get_context_data(**kwargs)  # получаем контекст уже существующий
+    # def get_context_data(self, *, object_list=None, **kwargs):
+        # context = super().get_context_data(**kwargs)  # получаем контекст уже существующий
         # c = Category.objects.get(slug=self.kwargs['cat_slug'])
         # c_def = self.get_user_context(
         #     title='Категория - ' + str(c.name),
         #     cat_selected=c.pk
         # )
-        print('get_context_data')
+        # print('get_context_data')
         # print(c_def)
         # return dict(list(context.items()) + list(c_def.items()))
-        return dict(list(context.items()))
+        # return dict(list(context.items()))
